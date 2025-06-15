b69be1f606bb3d1b973cb109dc715c8fff6b0fa2
Fumiaki Tokuhisa <1326825+tokuhisa@users.noreply.github.com>
2025-06-15 13:41:27 +0900

    Import api routing

---
+++ gendless-api-worker/src/api/getVersion.ts
@@ -0,0 +1,5 @@
+import { Hono } from 'hono';
+
+export const getVersion = new Hono().get('/version', (c) => {
+  return c.json({ version: "0.0.1" });
+});
--- gendless-api-worker/src/index.ts
+++ gendless-api-worker/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
+import { getVersion } from './api/getVersion';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/api/hono', (c) => {
   return c.json({ name: 'Hono!' })
-})
+});
+app.route('/api', getVersion);
 
 export default app
