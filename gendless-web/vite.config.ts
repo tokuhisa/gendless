18fdaa3f0e18c66e42f4de4cf78064e86e606a1b
Fumiaki Tokuhisa <1326825+tokuhisa@users.noreply.github.com>
2025-06-15 22:02:39 +0900

    Setup tailwindcss

--- gendless-web/vite.config.ts
+++ gendless-web/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import { createViteLicensePlugin } from 'rollup-license-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare(), createViteLicensePlugin()
+  plugins: [react(), tailwindcss(), cloudflare(), createViteLicensePlugin()
   ],
 })
